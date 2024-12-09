595c76360b82ad701e715175de5a71e33a11c75c
lll20031230 <>
2024-12-09 18:48:05 +0800

    刘雷雷

--- 1.初识C语言/选择语句和循环语句.c
+++ 1.初识C语言/选择语句和循环语句.c
@@ -3,22 +3,35 @@ int main()
 {
     int today = 2024;
     int birthday = 0;
-    printf("请输入您的生日年份：");
-    scanf("%d",&birthday);
-    if (birthday<0)
+    int same = 0;
+    while (same <= 3)
     {
-        printf("您输入的值错误！");
-    }
-    else
-    {
-        if(today - birthday >= 18)
+        if (same == 3)
         {
-            printf("您已成年！可尽情享受游戏时光！");
+            printf("您的次数已用完，请明天再试！\n");
         }
         else
         {
-            printf("您未成年！游戏时间将受到限制！");
+            printf("请输入您的出生年份：");
+            scanf("%d",&birthday);
+            if (birthday <= 0)
+            {
+                printf("请您输入正确年份！\n");
+            }
+            else
+            {
+                if (today - birthday >= 18)
+                {
+                    printf("您已成年，请尽情享受游戏时光！\n");
+                }
+                else
+                {
+                    printf("您未成年，将限制您的游戏时间！\n");
+                }
+                same+=3;
+            }
         }
+        same++;
     }
     return 0;
 }
