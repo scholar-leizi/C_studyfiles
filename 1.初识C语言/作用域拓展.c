cd933f53198088757fde670d30fa1c79c17e58ad
lll20031230 <>
2025-03-26 17:02:44 +0800

    刘雷雷

--- 1.初识C语言/作用域拓展.c
+++ 1.初识C语言/作用域拓展.c
@@ -1,1 +1,7 @@
 int d = 40;
+//static extern int d;//static修饰全局变量时，这个全局变量的外部链接属性会变成内部链接属性，其他源文件（.c）就不能在使用这个全局变量
+int add(int x,int y)
+{
+    return x + y;
+}
+//static extern add(int x,int y);//static修饰函数时，这个函数的外部链接属性会变成内部链接属性，其他源文件（.c）不能再调用这个函数
--- 1.初识C语言/关键字typedef和static.c
+++ 1.初识C语言/关键字typedef和static.c
@@ -13,14 +13,21 @@ void test()
     static int a = 0;//a被保留，出函数后没有被销毁，每次调用会延续上次保留的值
     //static修饰局部变量时，即使局部变量出了作用域，仍然被保留不销毁；static修饰局部变量时改变了局部变量的存储位置（栈区——>静态区）
     a++;
-    printf("%d ",a);
-
+    printf("%d\n",a);
 }
+
+extern int d;//声明调用工程内的全局变量（本源文件外的全局变量）
+
+extern int add(int x,int y);//声明调用源文件外部函数（本工程内）
+//函数也具有外部链接属性
+
 int main()
 {
     unsigned int num1 = 0;//“unsigned int”字母太多，编写不方便，重命名为uint
     //无符号整数num1
     uint num2 = 1;//与“unsigned int num1 = 0;”的数据类型一样
+
+    printf("%d\n",d);
 
     int i = 0;
     while(i<10)
@@ -28,5 +35,8 @@ int main()
         test();
         i++;
     }
+
+    printf("%d\n",add(10,20));
+    register int num = 3;//建议计算机把变量num储存到寄存器
     return 0;
 }
